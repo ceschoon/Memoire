6c57e633468b29b7b99d791934490700ae0aac6d
ceschoon <ceschoon@ulb.ac.be>
2020-05-28 15:03:51 +0200

    Data: more results for FCC-HCP stability

--- Cedric_Memoire_Data/WHDF_rc2_comparison_FCC_HCP/run_fixedkTMu.sh
+++ Cedric_Memoire_Data/WHDF_rc2_comparison_FCC_HCP/run_fixedkTMu.sh
@@ -6,30 +6,86 @@ cd fixedkTMu
 ### compute HCP 
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_HCP1.dat
-mv log.dat log_HCP1.dat
+	../input_files/input_fixedkTMu_HCP04.dat
+mv log.dat log_HCP04.dat
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_HCP2.dat
-mv log.dat log_HCP2.dat
+	../input_files/input_fixedkTMu_HCP05.dat
+mv log.dat log_HCP05.dat
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_HCP3.dat
-mv log.dat log_HCP3.dat
+	../input_files/input_fixedkTMu_HCP06.dat
+mv log.dat log_HCP06.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP07.dat
+mv log.dat log_HCP07.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP08.dat
+mv log.dat log_HCP08.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP09.dat
+mv log.dat log_HCP09.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP10.dat
+mv log.dat log_HCP10.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP11.dat
+mv log.dat log_HCP11.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP12.dat
+mv log.dat log_HCP12.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_HCP13.dat
+mv log.dat log_HCP13.dat
 
 ### compute FCC 
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_FCC1.dat
-mv log.dat log_FCC1.dat
+	../input_files/input_fixedkTMu_FCC04.dat
+mv log.dat log_FCC04.dat
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_FCC2.dat
-mv log.dat log_FCC2.dat
+	../input_files/input_fixedkTMu_FCC05.dat
+mv log.dat log_FCC05.dat
 
 ../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
-	../input_files/input_fixedkTMu_FCC3.dat
-mv log.dat log_FCC3.dat
+	../input_files/input_fixedkTMu_FCC06.dat
+mv log.dat log_FCC06.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC07.dat
+mv log.dat log_FCC07.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC08.dat
+mv log.dat log_FCC08.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC09.dat
+mv log.dat log_FCC09.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC10.dat
+mv log.dat log_FCC10.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC11.dat
+mv log.dat log_FCC11.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC12.dat
+mv log.dat log_FCC12.dat
+
+../../../Cedric_Memoire_Code/fixedkTMu/fixedkTMu \
+	../input_files/input_fixedkTMu_FCC13.dat
+mv log.dat log_FCC13.dat
 
 
 
