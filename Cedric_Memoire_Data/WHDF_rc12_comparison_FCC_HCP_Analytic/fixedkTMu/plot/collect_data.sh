0798acb7df2ee135227f8bef420233a031b318aa
ceschoon <ceschoon@ulb.ac.be>
2020-08-10 18:13:14 +0200

    Correct comparison FCC-HCP

--- Cedric_Memoire_Data/WHDF_rc12_comparison_FCC_HCP_Analytic/fixedkTMu/plot/collect_data.sh
+++ Cedric_Memoire_Data/WHDF_rc12_comparison_FCC_HCP_Analytic/fixedkTMu/plot/collect_data.sh
@@ -1,5 +1,42 @@
 #! /bin/bash
 
+# save original data 
+
+zip -r ../dataFCC.zip ../dataFCC
+zip -r ../dataHCP.zip ../dataHCP
+
+
+
+# remove unsuccessful computations
+
+for file in $(ls ../dataFCC/ | grep kTMu)
+do
+	success_str=$(cat ../dataFCC/$file | grep success)
+	unsuccessful_str="success = 0"
+	
+	if [ "$success_str" = "$unsuccessful_str" ]
+	then
+		rm -v ../dataFCC/$file
+		rm -v ../dataHCP/$file
+	fi
+done
+
+for file in $(ls ../dataHCP/ | grep kTMu)
+do
+	success_str=$(cat ../dataHCP/$file | grep success)
+	unsuccessful_str="success = 0"
+	
+	if [ "$success_str" = "$unsuccessful_str" ]
+	then
+		rm -v ../dataFCC/$file
+		rm -v ../dataHCP/$file
+	fi
+done
+
+
+
+# collect data in seperate files for each property 
+
 outFile="datakT.dat"
 echo "# temperatures for WHDF rc=1.2" > $outFile
 echo "" >> $outFile
@@ -22,6 +59,10 @@ outFile="dataHCP.dat"
 echo "# HCP free energies for WHDF rc=1.2" > $outFile
 echo "" >> $outFile
 cat ../dataHCP/kTMuSolid_kT\=* | grep freeEnergy >> $outFile
+
+
+
+# group in one file the data of each property
 
 paste datakT.dat dataMu.dat dataFCC.dat dataHCP.dat > data.dat
 
